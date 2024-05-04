47cae82df5d10943efbf300a4d7cbedbf5512464
RR <2r.reiyhes@gmail.com>
2024-05-04 19:39:34 +0800

    Addded login function

--- screens/Login.js
+++ screens/Login.js
@@ -1,146 +1,227 @@
-import { View, Text, Image , Pressable, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  Image,
+  Pressable,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import COLORS from '../constant';
+import COLORS from "../constant";
 import { Ionicons } from "@expo/vector-icons";
-import Checkbox from "expo-checkbox"
-import Button from '../components/Button';
-import { useNavigation } from '@react-navigation/native';
+import Checkbox from "expo-checkbox";
+import Button from "../components/Button";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
-const Login = ({ navigation }) => {
-    const navigations = useNavigation();
-    const [isPasswordShown, setIsPasswordShown] = useState(true);
-    const [isChecked, setIsChecked] = useState(false);
-    
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary2 }}>
-            <View style={{ flex: 1, marginHorizontal: 22,  }}>
-                <View style={{ marginVertical: 22 }}>
-                    <Text style={{
-                        fontSize: 22,
-                        fontWeight: 'bold',
-                        marginVertical: 50,
-                        color: COLORS.black
-                    }}>
-                        Hi Welcome Back! 👋
-                    </Text>
-                </View>
+function Login() {
+  const navigation = useNavigation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPasswordShown, setIsPasswordShown] = useState(true);
+  const [isChecked, setIsChecked] = useState(false);
+  const [error, setError] = useState(null);
 
-                <View style={{ marginBottom: 12 }}>
-                    <Text style={{
-                        fontSize: 16,
-                        fontWeight: 400,
-                        marginVertical: 8
-                    }}>Username</Text>
+  function loginSubmit({ props }) {
+    console.log(username, password);
 
-                    <View style={{
-                        width: "100%",
-                        height: 48,
-                        borderColor: COLORS.black,
-                        borderWidth: 1,
-                        borderRadius: 8,
-                        alignItems: "center",
-                        justifyContent: "center",
-                        paddingLeft: 22
-                    }}>
-                        <TextInput
-                            placeholder='Enter your username'
-                            placeholderTextColor={COLORS.black}
-                            keyboardType='email-address'
-                            style={{
-                                width: "100%"
-                            }}
-                        />
-                    </View>
-                </View>
+    const userData = {
+      username: username,
+      password: password,
+    };
 
-                <View style={{ marginBottom: 12 }}>
-                    <Text style={{
-                        fontSize: 16,
-                        fontWeight: 400,
-                        marginVertical: 8
-                    }}>Password</Text>
+    axios
+      .post("http://192.168.101.16:5000/login-user", userData)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.status === "ok") {
+          Alert.alert("Logged in");
+          navigation.navigate("Home");
+        } else {
+          setError("Login Failed");
+        }
+      })
+      .catch((error) => {
+        console.error("Axios Error:", error);
+        setError("Network error occurred");
+      });
+  }
+  // const handleLogin = async () => {
 
-                    <View style={{
-                        width: "100%",
-                        height: 48,
-                        borderColor: COLORS.black,
-                        borderWidth: 1,
-                        borderRadius: 8,
-                        alignItems: "center",
-                        justifyContent: "center",
-                        paddingLeft: 22
-                    }}>
-                        <TextInput
-                            placeholder='Enter your password'
-                            placeholderTextColor={COLORS.black}
-                            secureTextEntry={isPasswordShown}
-                            style={{
-                                width: "100%"
-                            }}
-                        />
+  //   console.log(username, password);
 
-                        <TouchableOpacity
-                            onPress={() => setIsPasswordShown(!isPasswordShown)}
-                            style={{
-                                position: "absolute",
-                                right: 12
-                            }}
-                        >
-                            {
-                                isPasswordShown == true ? (
-                                    <Ionicons name="eye-off" size={24} color={COLORS.black} />
-                                ) : (
-                                    <Ionicons name="eye" size={24} color={COLORS.black} />
-                                )
-                            }
+  //   try {
+  //     const response = await fetch("http://localhost:5000/api/login", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         username: username,
+  //         password: password,
+  //       }),
+  //     });
+  //     if (!response.ok) {
+  //       throw new Error("Invalid credentials");
+  //     }
+  //     // Navigate to home screen if login is successful
+  //     navigation.navigate("Home");
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  // };
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary2 }}>
+      <View style={{ flex: 1, marginHorizontal: 22 }}>
+        <View style={{ marginVertical: 22 }}>
+          <Text
+            style={{
+              fontSize: 22,
+              fontWeight: "bold",
+              marginVertical: 50,
+              color: COLORS.black,
+              textAlign: "center",
+              justifyContent: "center",
+            }}
+          >
+            Hi! Welcome Back! 👋
+          </Text>
+        </View>
 
-                        </TouchableOpacity>
-                    </View>
-                </View>
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 400,
+              marginVertical: 8,
+            }}
+          >
+            Username
+          </Text>
 
-                <View style={{
-                    flexDirection: 'row',
-                    marginVertical: 6
-                }}>
-                    <Checkbox
-                        style={{ marginRight: 8 }}
-                        value={isChecked}
-                        onValueChange={setIsChecked}
-                        color={isChecked ? COLORS.primary : undefined}
-                    />
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your username"
+              placeholderTextColor={COLORS.black}
+              keyboardType="email-address"
+              style={{
+                width: "100%",
+              }}
+              value={username}
+              onChangeText={(text) => setUsername(text)}
+            />
+          </View>
+        </View>
 
-                    <Text>Remenber Me</Text>
-                </View>
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 400,
+              marginVertical: 8,
+            }}
+          >
+            Password
+          </Text>
 
-                <Button
-                    onPress={() => navigation.navigate('Home')}
-                    title="Login"
-                    filled
-                    style={{
-                        marginTop: 18,
-                        marginBottom: 4,
-                    }}
-                />
-                <View style={{
-                    flexDirection: "row",
-                    justifyContent: "center",
-                    marginVertical: 22
-                }}>
-                     <Image
-                            source={require("../assets/images/logo.png")}
-                            style={{
-                                height: 350,
-                                width: 350,
-                                marginTop: 35,
-                                opacity: 0.6
-                            }}
-                            resizeMode='contain'
-                        />
-                </View>
-            </View>
-        </SafeAreaView>
-    )
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your password"
+              placeholderTextColor={COLORS.black}
+              secureTextEntry={isPasswordShown}
+              style={{
+                width: "100%",
+              }}
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+
+            <TouchableOpacity
+              onPress={() => setIsPasswordShown(!isPasswordShown)}
+              style={{
+                position: "absolute",
+                right: 12,
+              }}
+            >
+              {isPasswordShown == true ? (
+                <Ionicons name="eye-off" size={24} color={COLORS.black} />
+              ) : (
+                <Ionicons name="eye" size={24} color={COLORS.black} />
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginVertical: 6,
+          }}
+        >
+          <Checkbox
+            style={{ marginRight: 8 }}
+            value={isChecked}
+            onValueChange={setIsChecked}
+            color={isChecked ? COLORS.primary : undefined}
+          />
+
+          <Text>Remenber Me</Text>
+        </View>
+
+        <Button
+          onPress={loginSubmit}
+          title="Login"
+          filled
+          style={{
+            marginTop: 18,
+            marginBottom: 4,
+          }}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginVertical: 22,
+          }}
+        >
+          <Image
+            source={require("../assets/images/logo.png")}
+            style={{
+              height: 350,
+              width: 350,
+              marginTop: 35,
+              opacity: 0.6,
+            }}
+            resizeMode="contain"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
-export default Login
+export default Login;
